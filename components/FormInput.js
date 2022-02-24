f326ac5fde074867ffc85452465576ea4c6aaa74
JChris246 <chris.401@live.com>
2022-02-24 17:27:08 -0400

    feat:😎add dark styles for login page

--- components/FormInput.js
+++ components/FormInput.js
@@ -2,12 +2,12 @@ const FormInput = ({ label, onChange, value, name, type="text" }) => {
     return (
         <div className="mt-5 mx-0">
             <label className="pointer-events-none" htmlFor={name}>
-                <span className="text-lg block text-zinc-800">{label}</span>
+                <span className="text-lg block dark:text-zinc-800 dark:text-zinc-100">{label}</span>
             </label>
             <input
                 className="bg-transparent border-2 border-solid border-neutral-200 block w-full py-1 px-2
-                    text-lg text-zinc-800 focus:outline-none focus:border-blue-300 valid:outline-none
-                    valid:border-sky-600 rounded-md"
+                    text-lg text-zinc-800 dark:text-zinc-100 focus:outline-none focus:border-blue-300
+                    valid:outline-none valid:border-sky-600 rounded-md"
                 type={type} name={name} onChange={onChange} value={value}
                 required
             />
--- components/LoginTilt.js
+++ components/LoginTilt.js
@@ -1,11 +1,15 @@
+import { useTheme } from "next-themes";
 import styles from "../styles/LoginTilt.module.css"
 
 const LoginTilt = () => {
+    const { theme } = useTheme();
+
     return (
         <div className={styles.tilt}>
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
                 preserveAspectRatio="none">
-                <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className={styles.shapeFill}></path>
+                <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+                    className={ theme === "light" ? styles.shapeFill : styles.shapeFillDark }></path>
             </svg>
         </div>
     )
